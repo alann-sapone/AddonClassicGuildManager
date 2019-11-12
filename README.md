f3bfcc912662516f6be032f77fee09bad9e2840d
Alann Sapone <alann.sapone@gmail.com>
2019-11-12 19:52:48 +0100

    v0.1 ready

--- README.md
+++ README.md
@@ -10,9 +10,15 @@ The classic guild manager addon lets you export your character data to the [clas
 
 ### Getting Started
  
-Getting started is simple, first download the addon here and install it. On the character you want to export inventory for, simply type /cgm. The addon will generate a serialized data string which is then copied into the upload portion of the website.
+Getting started is simple, first download the addon here and install it. On the character you want to export inventory for, simply type
+
+> /cgm
+The addon will generate a serialized data string which is then copied into the upload portion of the website.
 Once uploaded the site will deserialize the string and your character data will be updated.
 
+> /cgm all
+To provide additionnal informations about the whole guild, and not just your current character.
+Please note that it may take a lot more time to load and your game may freeze (for a whole minute on big guilds...)
 
 ### Contact
  
--- main.lua
+++ main.lua
@@ -1,22 +1,25 @@
 ClassicGuildManager = LibStub('AceAddon-3.0'):NewAddon('ClassicGuildManager', 'AceConsole-3.0')
 AceConsole = LibStub('AceConsole-3.0', true)
-AceSerializer = LibStub('AceSerializer-3.0', true)
+
+VERSION = 0.1
 
 function ClassicGuildManager:OnInitialize()
   ClassicGuildManager:RegisterChatCommand('cgm', 'HandleChatCommand');
 end
 
 function ClassicGuildManager:HandleChatCommand(input)
-  ClassicGuildManager:DisplayExportString(exportString)
+  ClassicGuildManager:DisplayExportString(exportString, input == "all")
 end
 
 function ClassicGuildManager:GetCharacterData()
   local name = UnitName('player')
+  local _, englishClass = UnitClass('player')
+
   return {
     realm = GetRealmName(),
     name = name,
     race = UnitRace('player'),
-    class = UnitClass('player'),
+    class = englishClass,
     guildName = GetGuildInfo('player'),
     sex = UnitSex('player'),
     stuff = ClassicGuildManager:GetPlayerStuff()
@@ -51,20 +54,23 @@ function ClassicGuildManager:GetPlayerStuff()
 end
 
 function ClassicGuildManager:GetMemberData(memberIndex)
-  local name, rank, rankIndex, level, class, zone, note, officernote = GetGuildRosterInfo(memberIndex)
+  local name, rank, rankIndex, level, class, zone, note, officernote, _, _, classFileName = GetGuildRosterInfo(memberIndex)
+  local nameSuffix = "-" .. GetRealmName()
+  name = name:gsub("%" .. nameSuffix, "")
+
   return {
     name = name,
     rank = rank,
     rankIndex = rankIndex,
     level = level,
-    class = class,
+    class = classFileName,
     zone = zone,
-    note = note,
-    officernote = officernote
+    note = note:gsub("%\\", "|"):gsub("%\"", "\\\""),
+    officerNote = officernote
   }
 end
 
-function ClassicGuildManager:GetGuidMembers()
+function ClassicGuildManager:GetGuildMembers()
   local count = GetNumGuildMembers()
   local members = {}
 
@@ -75,26 +81,30 @@ function ClassicGuildManager:GetGuidMembers()
   return members
 end
 
-function ClassicGuildManager:DisplayExportString(exportString)
+function ClassicGuildManager:DisplayExportString(exportString, guildToo)
   local charData = ClassicGuildManager:GetCharacterData()
-  local guildData = ClassicGuildManager:GetGuidMembers()
+  local guildData = {}
+  
+  if guildToo then
+    guildData = ClassicGuildManager:GetGuildMembers()
+  end
 
   local data =  {
     character = charData,
-    guild = guildData
+    guild = guildData,
+    version = VERSION
   }
 
-  local serialized = LibAceSerializer:Serialize(data)
+  local serialized = Serialize(data)
 
   CgmFrame:Show()
   CgmFrameScroll:Show()
   CgmFrameScrollText:Show()
-  CgmFrameScrollText:SetText("test")
+  CgmFrameScrollText:SetText(encBase64(serialized))
   CgmFrameScrollText:HighlightText()
   
   CgmFrameButton:SetScript('OnClick', function(self)
     CgmFrame:Hide();
   end);
 
-  AceConsole:Print("TEST");
 end
